8ea184977c17b2fe3ccad0b385a7940816dbbd10
Your Name <partin.imeri@gmail.com>
2015-03-12 14:52:00 +0100

    Fixed training form requirements

--- app/modules/public/mod_apply_for_training/form.py
+++ app/modules/public/mod_apply_for_training/form.py
@@ -1,11 +1,12 @@
 from flask_wtf import Form
 from wtforms import StringField, DateField, TextAreaField
 from wtforms.validators import DataRequired, Length
-import datetime
+
+
 class RegisterTraining(Form):
     title = StringField(
         'title',
-        validators=[DataRequired(), Length(min=6, max=40)])
+        validators=[DataRequired(), Length(max=40)])
 
     startDate = DateField(
         'startDate', format="%Y-%m-%d",
@@ -17,27 +18,27 @@ class RegisterTraining(Form):
 
     space = StringField(
         'space',
-        validators=[DataRequired(), Length(min=6, max=40)])
+        validators=[DataRequired(), Length(max=40)])
 
     instructorName = StringField(
         'instructorName',
-        validators=[DataRequired(), Length(min=6, max=40)])
+        validators=[DataRequired(), Length( max=40)])
 
     instructorSurname = StringField(
         'instructorSurname',
-        validators=[DataRequired(), Length(min=6, max=40)])
+        validators=[DataRequired(), Length( max=40)])
 
     agenda = TextAreaField(
         'agenda',
-        validators=[DataRequired(), Length(min=6, max=100)])
+        validators=[DataRequired(), Length(max=100)])
 
     description = TextAreaField(
         'description',
-        validators=[DataRequired(), Length(min=6, max=100)])
+        validators=[DataRequired()])
 
     requirements = TextAreaField(
         'requirements',
-        validators=[DataRequired(), Length(min=6, max=100)])
+        validators=[DataRequired(), Length(max=100)])
 
     def validate(self):
         initial_validation = super(RegisterTraining, self).validate()
