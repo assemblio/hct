f2dcb8bb3586effa74a850b7e27e120c9a97fbc1
Your Name <partin.imeri@gmail.com>
2015-03-19 13:20:21 +0100

    Listed users and jobs in Admin section

--- app/modules/admin/users/views.py
+++ app/modules/admin/users/views.py
@@ -1,12 +1,19 @@
-from flask import render_template, Blueprint
+from flask import render_template, Blueprint, request
 from flask.ext.security import login_required, roles_required
-
+from app.modules.public.mod_authentication.user_registration.model import User
 # Define the blueprint:
 admin_users = Blueprint('admin_users', __name__)
 
 
-@admin_users.route('/admin/users')
+@admin_users.route('/admin/users', methods=['GET', 'POST'])
 @login_required
 @roles_required('Admin')
 def index():
-    return render_template('admin/users/users.html')
+    if not request.args.get('page'):
+        page = 1
+    else:
+        page = int(request.args.get('page'))
+    useri = User.objects.all()
+    #page = request.get('page')
+    pagination = useri.paginate(page=page, per_page=10)
+    return render_template('admin/users/users.html', pagination=pagination)
